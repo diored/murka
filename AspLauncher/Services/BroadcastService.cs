ee3ce2a7bc05bf60586960ee84757af4ac2c785e
Dio.RED <38877612+diored@users.noreply.github.com>
2023-08-05 15:26:32 +0300

    Add logging to gRPC

--- AspLauncher/Services/BroadcastService.cs
+++ AspLauncher/Services/BroadcastService.cs
@@ -10,14 +10,17 @@ namespace DioRed.Murka.AspLauncher.Services;
 public class BroadcastService : Broadcast.BroadcastBase
 {
     private readonly VermilionManager _botManager;
+    private readonly ILogger _logger;
 
-    public BroadcastService(VermilionManager botManager)
+    public BroadcastService(VermilionManager botManager, ILogger<BroadcastService> logger)
     {
         _botManager = botManager;
+        _logger = logger;
     }
 
     public override async Task<Empty> Agenda(Empty request, ServerCallContext context)
     {
+        _logger.LogInformation("Someone uses gRPC request: Agenda");
         await _botManager.Broadcast(DailyAgenda);
         return new Empty();
     }
