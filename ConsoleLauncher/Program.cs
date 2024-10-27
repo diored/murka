e30481f326b26887a5c61d5a3865d2b3db97dbc5
Dio.RED <38877612+diored@users.noreply.github.com>
2024-10-27 11:01:17 +0200

    Use Vermilion.Hosting extensions

--- ConsoleLauncher/Program.cs
+++ ConsoleLauncher/Program.cs
@@ -1,50 +1,19 @@
-using DioRed.Common.AzureStorage;
-using DioRed.Common.Logging;
 using DioRed.Murka.Core.Entities;
-using DioRed.Vermilion;
-using DioRed.Vermilion.ChatStorage;
-using DioRed.Vermilion.Subsystems.Telegram;
+using DioRed.Vermilion.Hosting;
 
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+Defaults.ConsoleLoggerTimeZone = CommonValues.ServerTimeZoneShift;
+
 IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureLogging(logging => logging.UseDioRedLogging(
-        "Murka",
-        options =>
-        {
-            options.EventColors.Add(Events.JobStarted, "mediumspringgreen");
-            options.EventColors.Add(Events.JobFinished, "mediumspringgreen");
-            options.EventColors.Add(Events.JobScheduled, "mediumspringgreen");
-
-            options.DateTimeOffset = CommonValues.ServerTimeZoneShift;
-
-            options.ExceptionFormat = Spectre.Console.ExceptionFormats.ShortenEverything;
-        }
-    ))
-    .ConfigureServices((context, services) => services
-        .AddMurkaDependencies(context.Configuration)
-        .AddVermilion(builder => builder
-            .UseAzureTableChatStorage(AzureStorageSettings.MicrosoftAzure(
-                accountName: ReadRequired(context.Configuration, "Vermilion:AzureTable:AccountName"),
-                accountKey: ReadRequired(context.Configuration, "Vermilion:AzureTable:AccountKey")
-            ))
-            .AddCommandHandlersFromAssembly(typeof(MurkaServicesExtension).Assembly)
-            .AddTelegram()
-        )
-    )
-    .Build();
+    .BuildDefaultVermilionHost(
+        botName: "Murka",
+        configureServices: (context, services) => services.AddMurkaDependencies(context.Configuration),
+        assembliesWithHandlers: [typeof(MurkaServicesExtension).Assembly]
+    );
 
 host.Services.SetupMurkaJobs();
 
 await host.RunAsync();
 return;
-
-static string ReadRequired(
-    IConfiguration configuration,
-    string keyName
-)
-{
-    return configuration[keyName] ?? throw new InvalidOperationException($"""Cannot read "{keyName}" value""");
-}
