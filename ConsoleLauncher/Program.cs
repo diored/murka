fc875b1edb1f029a0e6cf0eb97c12768fa97cf90
Dio.RED <38877612+diored@users.noreply.github.com>
2024-11-08 11:44:18 +0200

    Update assembly locator

--- ConsoleLauncher/Program.cs
+++ ConsoleLauncher/Program.cs
@@ -7,7 +7,7 @@ IHost host = Host.CreateDefaultBuilder(args)
     .BuildDefaultVermilionHost(
         botName: "Murka",
         configureServices: (context, services) => services.AddMurkaDependencies(context.Configuration),
-        assemblies: [AppDomain.CurrentDomain.GetAssemblies().First(a => a.FullName?.StartsWith("Murka.Core,") == true)]
+        assemblies: [AppDomain.CurrentDomain.GetAssemblies().First(a => a.FullName?.Contains("Murka.Core") == true)]
     );
 
 await host.RunAsync();
