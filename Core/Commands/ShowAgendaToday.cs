fd779cc7aa66766d30e1e3d29dc3e6fc58e8a3dd
Dio.RED <38877612+diored@users.noreply.github.com>
2025-01-11 08:38:48 +0200

    Fix the template handler

--- Core/Commands/ShowAgendaToday.cs
+++ Core/Commands/ShowAgendaToday.cs
@@ -6,6 +6,6 @@ namespace DioRed.Murka.Core.Commands;
 public class ShowAgendaToday(ILogic logic)
     : ShowAgendaBase(logic, template, getDateFunc)
 {
-    private static readonly Template template = new[] { "/daily", "сводка" };
+    private static readonly Template template = new[] { "/agenda", "сводка" };
     private static readonly Func<DateOnly> getDateFunc = () => ServerDateTime.GetCurrent().Date;
 }
